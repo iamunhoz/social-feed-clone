2fc538ce4b8eb136fc24d0747b26bbe2c932a362
Ivã Munhoz <iamunhoz@gmail.com>
2021-05-31 09:54:08 -0300

    unstable: followgrid

--- client/user/DeleteUser.js
+++ client/user/DeleteUser.js
@@ -13,56 +13,56 @@ import auth from './../auth/auth-helper'
 import { Redirect } from 'react-router-dom'
 
 export default function DeleteUser(props) {
-	const [open, setOpen] = useState(false)
-	const [redirect, setRedirect] = useState(false)
-	const jwt = auth.isAuthenticated()
+  const [open, setOpen] = useState(false)
+  const [redirect, setRedirect] = useState(false)
+  const jwt = auth.isAuthenticated()
 
-	const clickButton = () => setOpen(true)
+  const clickButton = () => setOpen(true)
 
-	const handleRequestClose = () => setOpen(false)
+  const handleRequestClose = () => setOpen(false)
 
-	const deleteAccount = () => {
-		remove(
-			{ userId: props.userId },
-			{ t: jwt.token }
-		).then(data => {
-			if (data && data.error) {
-				throw new Error('Unable to delete account')
-			} else {
-				auth.clearJwt(() => console.log('account deleted'))
-				setRedirect(true)
-			}
-		})
-	}
+  const deleteAccount = () => {
+    remove(
+      { userId: props.userId },
+      { t: jwt.token }
+    ).then(data => {
+      if (data && data.error) {
+        throw new Error('Unable to delete account')
+      } else {
+        auth.clearJwt(() => console.log('account deleted'))
+        setRedirect(true)
+      }
+    })
+  }
 
-	if (redirect) {
-		return <Redirect to='/' />
-	} else {
-		return (<span>
-			<IconButton
-				arial-label="delete"
-				color='secondary'
-				onClick={clickButton}>
-				<DeleteIcon/>
-			</IconButton>
+  if (redirect) {
+    return <Redirect to='/' />
+  } else {
+    return (<span>
+      <IconButton
+        arial-label="delete"
+        color='secondary'
+        onClick={clickButton}>
+        <DeleteIcon/>
+      </IconButton>
 
-			<Dialog open={open} onClose={handleRequestClose}>
-				<DialogTitle>Delete Account</DialogTitle>
-				<DialogContent>
-					<DialogContentText> Confirm to delete your account</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleRequestClose} color='primary'>
+      <Dialog open={open} onClose={handleRequestClose}>
+        <DialogTitle>Delete Account</DialogTitle>
+        <DialogContent>
+          <DialogContentText> Confirm to delete your account</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleRequestClose} color='primary'>
 						Cancel
-					</Button>
-					<Button onClick={deleteAccount} color='secondary' autoFocus='autoFocus'>
+          </Button>
+          <Button onClick={deleteAccount} color='secondary' autoFocus='autoFocus'>
 						Confirm
-					</Button>
-				</DialogActions>
-			</Dialog>
-		</span>)
-	}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </span>)
+  }
 }
 DeleteUser.propTypes = {
-	userId: PropTypes.string.isRequired
+  userId: PropTypes.string.isRequired
 }
--- client/user/Profile.js
+++ client/user/Profile.js
@@ -16,136 +16,163 @@ import auth from './../auth/auth-helper'
 import { read } from './api-user'
 import { Redirect, Link } from 'react-router-dom'
 import FollowProfileButton from './../user/FollowProfileButton'
+// import ProfileTabs from './ProfileTabs'
+// import { listByUser } from './../post/api-post'
 
 const useStyles = makeStyles(theme => ({
-	root: theme.mixins.gutters({
-		maxWidth: 600,
-		margin: 'auto',
-		padding: theme.spacing(3),
-		marginTop: theme.spacing(5),
-		backgroundColor: theme.palette.primary.main
-	}),
-	title: {
-		marginTop: theme.spacing(3),
-		color: theme.palette.protectedTitle
-	},
-	about: {
-		fontStyle: 'italic',
-		color: theme.palette.primary.contrastText
-	},
-	userInfo: {
-		maxWidth: '110px',
-		color: theme.palette.primary.contrastText
-	},
-	contrastText: {
-		color: theme.palette.primary.contrastText
-	}
+  root: theme.mixins.gutters({
+    maxWidth: 600,
+    margin: 'auto',
+    padding: theme.spacing(3),
+    marginTop: theme.spacing(5),
+    backgroundColor: theme.palette.primary.main
+  }),
+  title: {
+    marginTop: theme.spacing(3),
+    color: theme.palette.protectedTitle
+  },
+  about: {
+    fontStyle: 'italic',
+    color: theme.palette.primary.contrastText
+  },
+  userInfo: {
+    maxWidth: '110px',
+    color: theme.palette.primary.contrastText
+  },
+  contrastText: {
+    color: theme.palette.primary.contrastText
+  }
 }))
 
 export default function Profile({ match }) {
-	const classes = useStyles()
-	const [values, setValues] = useState({
-		user: { following: [], followers: [] },
-		redirectToSignin: false,
-		following: false
-	})
-	const jwt = auth.isAuthenticated()
+  const jwt = auth.isAuthenticated()
+  const classes = useStyles()
+  const [values, setValues] = useState({
+    user: { following: [], followers: [] },
+    redirectToSignin: false,
+    following: false
+  })
+  const [posts, setPosts] = useState([])
 
-	useEffect(() => {
-		const abortController = new AbortController()
-		const signal = abortController.signal
+/*   const loadPosts = (user) => {
+    listByUser(
+      { userId: user },
+      { t: jwt.token }
+    ).then(data => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        setPosts(data)
+      }
+    })
+  } */
 
-		read(
-			{ userId: match.params.userId },
-			{ t: jwt.token },
-			signal
-		).then(data => {
-			if (data && data.error) {
-				setValues(...values, { redirecToSignin: true })
-			} else {
-				const following = checkFollow(data)
-				setValues({ ...values, user: data, following: following })
-			}
-		})
+  const removePost = (post) => {
+    const updatedPosts = posts
+    const index = updatedPosts.indexOf(post)
+    updatedPosts.splice(index, 1)
+    setPosts(updatedPosts)
+  }
 
-		return function cleanup() {
-			abortController.abort()
-		}
-	}, [match.params.userId])
+  useEffect(() => {
+    const abortController = new AbortController()
+    const signal = abortController.signal
 
-	const clickFollowButton = callApi => {
-		callApi(
-			{ userId: jwt.user._id },
-			{ t: jwt.token },
-			values.user._id
-		).then(data => {
-			if (data.error) {
-				setValues({ ...values, error: data.error })
-			} else {
-				setValues({ ...values, user: data, following: !values.following })
-			}
-		})
-	}
+    read(
+      { userId: match.params.userId },
+      { t: jwt.token },
+      signal
+    ).then(data => {
+      if (data && data.error) {
+        setValues(...values, { redirecToSignin: true })
+      } else {
+        const following = checkFollow(data)
+        setValues({ ...values, user: data, following: following })
+        // loadPosts(data._id)
+      }
+    })
 
-	const checkFollow = user => {
-		const match = user.followers.some(follower => {
-			return follower._id == jwt.user._id
-		})
-		return match
-	}
+    return function cleanup() {
+      abortController.abort()
+    }
+  }, [match.params.userId])
 
-	const photoUrl = values.user._id ?
-		`/api/users/photo/${values.user._id}?${new Date().getTime()}` :
-		'/api/users/defaultphoto'
+  const clickFollowButton = callApi => {
+    callApi(
+      { userId: jwt.user._id },
+      { t: jwt.token },
+      values.user._id
+    ).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error })
+      } else {
+        setValues({ ...values, user: data, following: !values.following })
+      }
+    })
+  }
 
-	if (values.redirectToSignin) {
-		return <Redirect to='/signin' />
-	} else {
-		return (
-			<Paper className={classes.root} elevation={4}>
-				<Typography
-					variant='h6'
-					className={classes.title}>
+  const checkFollow = user => {
+    const match = user.followers.some(follower => {
+      return follower._id == jwt.user._id
+    })
+    return match
+  }
+
+  const photoUrl = values.user._id ?
+    `/api/users/photo/${values.user._id}?${new Date().getTime()}` :
+    '/api/users/defaultphoto'
+
+  if (values.redirectToSignin) {
+    return <Redirect to='/signin' />
+  } else {
+    return (
+      <Paper className={classes.root} elevation={4}>
+        <Typography
+          variant='h6'
+          className={classes.title}>
 						Profile
-				</Typography>
-				<List dense>
-					<ListItem>
-						<ListItemAvatar>
-							<Avatar src={photoUrl}/>
-						</ListItemAvatar>
-						<ListItemText
-							primary={values.user.name}
-							secondary={values.user.email}
-							className={classes.userInfo}
-						/>
-						<ListItemText
-							primary={values.user.about}
-							className={classes.about}
-						/>
-						{ auth.isAuthenticated().user && auth.isAuthenticated().user._id === values.user._id ?
-							(<ListItemSecondaryAction>
-								<Link to={'/user/edit/' + values.user._id}>
-									<IconButton arial-label='Edit' color='primary'>
-										<Edit className={classes.contrastText} />
-									</IconButton>
-								</Link>
-								<DeleteUser userId={values.user._id}/>
-							</ListItemSecondaryAction>)
-							:
-							(
-								<FollowProfileButton
-									following={values.following}
-									onButtonClick={clickFollowButton}
-								/>
-							)
-						}
-					</ListItem>
-					<Divider />
-					<ListItem>
-						<ListItemText primary={'Joined: ' + (new Date(values.user.created)).toDateString()} className={classes.contrastText}/>
-					</ListItem>
-				</List>
-			</Paper>
-		)
-	}
+        </Typography>
+        <List dense>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar src={photoUrl}/>
+            </ListItemAvatar>
+            <ListItemText
+              primary={values.user.name}
+              secondary={values.user.email}
+              className={classes.userInfo}
+            />
+            <ListItemText
+              primary={values.user.about}
+              className={classes.about}
+            />
+            { auth.isAuthenticated().user && auth.isAuthenticated().user._id === values.user._id ?
+              (<ListItemSecondaryAction>
+                <Link to={'/user/edit/' + values.user._id}>
+                  <IconButton arial-label='Edit' color='primary'>
+                    <Edit className={classes.contrastText} />
+                  </IconButton>
+                </Link>
+                <DeleteUser userId={values.user._id}/>
+              </ListItemSecondaryAction>)
+              :
+              (
+                <FollowProfileButton
+                  following={values.following}
+                  onButtonClick={clickFollowButton}
+                />
+              )
+            }
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary={'Joined: ' + (new Date(values.user.created)).toDateString()} className={classes.contrastText}/>
+          </ListItem>
+        </List>
+        
+      </Paper>
+    )
+  }
 }
+
+const ProfileTabs = '<ProfileTabs user={values.user} posts={posts} removePostUpdate={removePost} />'
