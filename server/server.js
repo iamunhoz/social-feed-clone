6340294b69eb893a612bd8e0eae39dddbf605b7a
Ivã Munhoz <iamunhoz@gmail.com>
2021-05-31 11:32:26 -0300

    colorful messages with chalk for the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import config from './../config/config'
 import app from './express'
 import mongoose from 'mongoose'
+import chalk from 'chalk'
 
 // Connection URL
 mongoose.Promise = global.Promise
@@ -9,9 +10,13 @@ mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
+mongoose.connection.on('connected', () => {
+  console.log(chalk.bold.green('Mongoose has connected to Mongodb Atlas'))
+})
+
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
   }
-  console.info('Server started on port %s.', config.port)
+  console.info(chalk.bold.blue(`Server has started and is listening to port ${config.port}`))
 })
