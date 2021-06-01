d2101ded57ea2892628378fffec4507ecb6db109
Ivã Munhoz <iamunhoz@gmail.com>
2021-06-01 18:39:00 -0300

    workin on 'Posts'

--- client/user/ProfileTabs.js
+++ client/user/ProfileTabs.js
@@ -5,7 +5,7 @@ import Typography from '@material-ui/core/Typography'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import FollowGrid from './FollowGrid'
-// import PostList from './../post/PostList' não foi feito ainda
+import PostList from './../post/PostList'
 
 export default function ProfileTabs() {
   const [tab, setTab] = useState(0)
