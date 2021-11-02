dbf4b0bdac244e1064db7888f971320a5af29e15
Ivã Munhoz <iamunhoz@gmail.com>
2021-11-02 10:35:11 -0300

    solving a heroku deploy bug

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
 const secrets = {
-  user: process.env.MONGO_USER,
-  password: process.env.MONGO_PASSWORD,
-  uri: process.env.MONGO_URI,
-  defaultDb: process.env.MONGO_DEFAULT_DB,
-  jwt: process.env.JWT_SECRET
+  user: 'admin-mern',
+  password: 'asumdois345',
+  uri: 'mern-projects.uuzhb.mongodb.net',
+  defaultDb: 'classicalPeople',
+  jwt: 'zubizarreta'
 }
 const mongoCredentials = `${secrets.user}:${secrets.password}@${secrets.uri}/${secrets.defaultDb}`
 
 const config = {
 	env: process.env.NODE_ENV || 'development',
-	port: process.env.PORT,
+	port: process.env.PORT || 8080,
 	jwtSecret: secrets.jwt,
 	mongoUri: 'mongodb+srv://' + mongoCredentials + '?retryWrites=true&w=majority'
 }
